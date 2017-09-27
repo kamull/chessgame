e13c782aff1d0d081b7522be9b7de1bcc03a2064
Kurt Mull <kurtmull@gmail.com>
2017-09-27 01:19:09 +0000

    set up chess board, html, and css

---
+++ src/js/initBoard.js
@@ -0,0 +1,10 @@
+function initBoard () {
+    console.log('did this exocute?')
+    
+    for(let i = 0; i > 8; i++) {
+       const row = document.createElement('div');
+    }
+    
+}
+
+export default initBoard;
